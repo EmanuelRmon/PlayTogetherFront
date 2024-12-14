2957dd2c6a574074758701a0187f66cd9df4c6c6
Camilo Uscategui <acuscategui30@gmail.com>
2024-12-13 20:26:28 -0500

    configuracion basica paso 15

--- PlayTogetherFront/src/app/app.routes.ts
+++ PlayTogetherFront/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ShopComponent } from './components/shop/shop.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component:HomeComponent},
+    { path: 'login', component:LoginComponent},
+    { path: 'register', component:RegisterComponent},
+    { path: 'shop', component:ShopComponent}
+];
