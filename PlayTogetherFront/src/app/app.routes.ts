398fdf744213d3725d230bd76ba6633004ec9065
Camilo Uscategui <acuscategui30@gmail.com>
2024-12-16 18:26:25 -0500

    actualizacion register y login

--- PlayTogetherFront/src/app/app.routes.ts
+++ PlayTogetherFront/src/app/app.routes.ts
@@ -5,8 +5,8 @@ import { RegisterComponent } from './components/register/register.component';
 import { ShopComponent } from './components/shop/shop.component';
 
 export const routes: Routes = [
-    { path: 'home', component:HomeComponent},
-    { path: 'login', component:LoginComponent},
-    { path: 'register', component:RegisterComponent},
-    { path: 'shop', component:ShopComponent}
+    { path: 'home', title: 'PlayTogether - Home', component:HomeComponent},
+    { path: 'login', title: 'PlayTogether - Login', component:LoginComponent},
+    { path: 'register',title: 'PlayTogether - Register',component:RegisterComponent},
+    { path: 'shop',title: 'PlayTogether - Shop', component:ShopComponent}
 ];
