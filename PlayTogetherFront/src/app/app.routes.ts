6dcf7f69c38375396135f0f760993e461b5ded0c
Camilo Uscategui <acuscategui30@gmail.com>
2024-12-20 13:44:21 -0500

    error 404 añadido

--- PlayTogetherFront/src/app/app.routes.ts
+++ PlayTogetherFront/src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ShopComponent } from './components/shop/shop.component';
+import { Error404Component } from './components/error404/error404.component';
 
 export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'login'},
     { path: 'home', title: 'PlayTogether - Home', component:HomeComponent},
     { path: 'login', title: 'PlayTogether - Login', component:LoginComponent},
     { path: 'register',title: 'PlayTogether - Register',component:RegisterComponent},
-    { path: 'shop',title: 'PlayTogether - Shop', component:ShopComponent}
+    { path: '404', component:Error404Component},
+    { path: 'shop',title: 'PlayTogether - Shop', component:ShopComponent},
+    { path: '**', pathMatch: 'full', redirectTo: '404'}
+
 ];
