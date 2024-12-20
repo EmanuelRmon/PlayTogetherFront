b0268eef85d7de80d12d5873b06bd2d11d3e584b
EmanuelRmon <emanuelrmonroy@gmail.com>
2024-12-20 00:28:48 -0500

    shop agregado

--- PlayTogetherFront/src/app/services/services/juegosmain.service.spec.ts
+++ PlayTogetherFront/src/app/services/services/juegosmain.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { JuegosmainService } from './juegosmain.service';
+import { JuegosmainService } from '../services/juegosmain.service';
 
 describe('JuegosmainService', () => {
   let service: JuegosmainService;
