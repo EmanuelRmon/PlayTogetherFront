10ad34366dd449a925cbd852553006b05aacc491
EmanuelRmon <emanuelrmonroy@gmail.com>
2025-02-10 18:06:25 -0500

    putongas

--- PlayTogetherFront/src/app/services/services/juegosmain.service.ts
+++ PlayTogetherFront/src/app/services/services/juegosmain.service.ts
@@ -1,51 +1,19 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD
-import { amazonURL } from '../../utils/localUrl';
-import { localUrl } from '../../utils/localUrl';
-
-=======
 import { localUrl } from '../../utils/localUrl';
 import { amazonUrl } from '../../utils/localUrl';
->>>>>>> main2
 
 @Injectable({
   providedIn: 'root'
 })
 export class JuegosmainService {
 
-<<<<<<< HEAD
-=======
 token = sessionStorage.getItem('token')
->>>>>>> main2
 
   constructor(private http: HttpClient) { }
 
 
 getFavoriteGames () {
-<<<<<<< HEAD
-    return this.http.get (`${amazonURL}/games`)
-  }
-
-deleteGame (id: string) {
-    return this.http.delete(`${amazonURL}/deletegame/${id}`)
-}
-
-addGame (body: any) {
-    return this.http.post(`${amazonURL}/addgame`, body)
-}
-
-updateGame (id :string, body:any) {
-    return this.http.put(`${amazonURL}/updategame/${id}`, body)
-}
-
-getGame (id :string) {
-    return this.http.get(`${amazonURL}/game/${id}`)
-}
-
-searchGame (name: string) {
-    return this.http.get (`${amazonURL}/games/${name}`)
-=======
     const headers = new HttpHeaders().set('Authorization', `Bearer ${this.token}`)
     return this.http.get (`${amazonUrl}/games`, {headers})
   }
@@ -73,6 +41,5 @@ getGame (id :string) {
 searchGame (name: string) {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${this.token}`)
     return this.http.get (`${amazonUrl}/games/${name}`, {headers})
->>>>>>> main2
 }
 }
